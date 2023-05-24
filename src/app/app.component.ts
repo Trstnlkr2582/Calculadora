e112ae67234bb4d76ba609ff703aeca1bbd98fcd
cbello0502 <carlosbeor@unisabana.edu.co>
2023-05-24 14:04:21 -0500

    casiMath

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Calculadora';
+  showMore :boolean = false
+  show(): void{
+    this.showMore = !this.showMore
+  }
 }
